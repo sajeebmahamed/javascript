82b4dbbd4b4daa82332456d97bc15dbae049e22e
Sajeeb <sajeebahamed6@gmail.com>
2020-02-26 19:50:07 +0600

    pushpop

---
+++ push_pop_shift_unshift_length.js
@@ -0,0 +1,13 @@
+var arr = ['a','b','c','d'];
+console.log(arr.length);
+arr.push('e');
+console.log(arr);
+
+arr.pop();
+console.log(arr);
+
+arr.shift();
+console.log("shift:",arr);
+
+arr.unshift('a');
+console.log("unshift", arr);
